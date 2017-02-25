a448b08e84bea0a68bf25aee6d54673613186060
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-02-26 03:55:47 +0900

    blog_seedにサイト名追加

--- SSConnectAPI/db/blog_seed.rb
+++ SSConnectAPI/db/blog_seed.rb
@@ -1,1 +1,8 @@
-Blog.create(:title => 'エレファント速報',)
+# coding: utf-8
+
+Blog.create(:title => 'エレファント速報',:url => 'http://elephant.2chblog.jp/',:rss => 'http://elephant.2chblog.jp/index.rdf')
+Blog.create(:title => 'えすえすログ',:url => 'http://s2-log.com',:rss => 'http://s2-log.com/index.rdf')
+Blog.create(:title => '森きのこ！',:url => 'http://morikinoko.com',:rss => 'http://morikinoko.com/index.rdf')
+Blog.create(:title => 'あやめ速報',:url => 'http://ayamevip.com/',:rss => 'http://ayamevip.com/index.rdf')
+Blog.create(:title => 'えすえすmode',:url => 'http://blog.livedoor.jp/mode_ss/',:rss => 'http://blog.livedoor.jp/mode_ss/index.rdf')
+
