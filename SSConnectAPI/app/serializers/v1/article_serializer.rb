173f19de6acfe6e28cdded5904c1b495c23cf578
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-03-02 16:28:34 +0900

    serializer変更

--- SSConnectAPI/app/serializers/v1/article_serializer.rb
+++ SSConnectAPI/app/serializers/v1/article_serializer.rb
@@ -1,3 +1,9 @@
 class ArticleSerializer < ActiveModel::Serializer
   attributes :id, :title, :blog_id
+
+  has_one :blog, serealizer:BlogSerializer
+
+  def articles_count
+    object.articles.count
+  end
 end
--- SSConnectAPI/app/serializers/v1/blog_serializer.rb
+++ SSConnectAPI/app/serializers/v1/blog_serializer.rb
@@ -1,6 +1,3 @@
 class BlogSerializer < ActiveModel::Serializer
   attributes :id, :title, :url, :rss
-
-  has_many :articles, serializer:ArticleSerializer
-
 end
