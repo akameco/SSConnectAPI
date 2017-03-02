ecb974db6893577c67f02e88cca04549d13b3203
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-03-02 15:45:27 +0900

    route.rb変更

--- SSConnectAPI/app/models/article.rb
+++ SSConnectAPI/app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
-  belongs_to :blog
+  belongs_to :blog, :foreign_key => blog_id
   acts_as_taggable # Alias for acts_as_taggable_on :tags
   acts_as_taggable_on :category
+
+  validates :title, presence: true
 end
