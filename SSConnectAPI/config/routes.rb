66de5674082b7df78e7e71788b681dd4049dd47d
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-03-03 10:33:39 +0900

    V1をv1に変更して落ちなくさせた

--- SSConnectAPI/config/routes.rb
+++ SSConnectAPI/config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #namespace :V1, defaults: {format: :json } do
+  namespace :v1, defaults: {format: :json } do
     resources :articles
-  #end
+    get '*path', controller: 'application', action: 'render_404'
+  end
 
 end
