a4e13a7acc4aa43dbdb0a015b6a0b055f3599b56
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-03-04 17:40:07 +0900

    seedでデータベース作成できるようにした

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'sample@cps.im.dendai.ac.jp', password: 'rubirubiSS', password_confirmation: 'rubirubiSS')
+
+10.times do |i|
+  blog = Blog.create(
+      :title => "TitleSokuho#{i}",
+      :url => Faker::Internet.url,
+      :rss => "#{Faker::Internet.url}/rss"
+  )
+  puts i
+  1000.times do |j|
+    blog.articles.create(
+        :title => "#{Faker::Food.ingredient} #{j}",
+        :url => Faker::Internet.url,
+        :posted_at => Faker::Time.between(2.years.ago, Date.today, :all)
+    )
+  end
+end
