b7d406073cc031273cae80e648815b5d4d31f774
elzup <guild0105@gmail.com>
2017-03-11 01:22:43 +0900

    Story table

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,4 @@
 class Article < ApplicationRecord
   belongs_to :blog, :foreign_key => :blog_id
-  acts_as_taggable # Alias for acts_as_taggable_on :tags
-  acts_as_taggable_on :category
-
-  validates :title, presence: true
+  belongs_to :story, :foreign_key => :story_id
 end
