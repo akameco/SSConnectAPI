87d008e0b8d1c11ac7514d272090fe285f21c608
elzup <guild0105@gmail.com>
2017-03-11 03:30:04 +0900

    Small fix

--- app/controllers/v1/articles_controller.rb
+++ app/controllers/v1/articles_controller.rb
@@ -1,21 +1,10 @@
 module V1
   class ArticlesController < ApplicationController
-    #     pages GET    /pages(.:format)          pages#index
-    #           POST   /pages(.:format)          pages#create
-    #  new_page GET    /pages/new(.:format)      pages#new
-    # edit_page GET    /pages/:id/edit(.:format) pages#edit
-    #      page GET    /pages/:id(.:format)      pages#show
-    #           PUT    /pages/:id(.:format)      pages#update
-    #           DELETE /pages/:id(.:format)      pages#destroy
 
     # articles GET
     # show_all_article
     def index
-      if params[:page].nil?
-        p = 1
-      else
-        p = params[:page].to_i
-      end
+      p = (params[:page] || 1).to_i
 
       if params[:blog_id].nil?
         articles = Article.all
